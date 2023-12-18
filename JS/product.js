694f3f19f69b31fe23de95311b1c7fbde98ff0f7
KHLORP.VEAK <KHLORP.VEAK@PNC.LAN>
2023-12-18 09:11:14 +0700

    update card

--- JS/product.js
+++ JS/product.js
@@ -6,10 +6,6 @@ let array = [];
 function save(){
     localStorage.setItem("array", JSON.stringify(array));
 };
-function load(){
-    array = JSON.parse(localStorage.getItem("array"));
-};  
-load()
 button.addEventListener('click', function(e){
     e.preventDefault()
     let textName = document.getElementById('textName');
