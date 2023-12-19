721e074a002fa4fd10768ccaa130ec60055e7c0a
KHLORP.VEAK <KHLORP.VEAK@PNC.LAN>
2023-12-19 14:49:06 +0700

    #5

--- JS/product.js
+++ JS/product.js
@@ -1,108 +1,127 @@
-let addcard = document.querySelector('.AddCard');
-let button = document.getElementById('Add-Prooduct');
-let sum = 0;
-//========Add Card========//
-let array = [];
-function save(){
-    localStorage.setItem("array", JSON.stringify(array));
+
+let deta_Product = [];
+function saveStorage() {
+    localStorage.setItem('deta_Product', JSON.stringify(deta_Product));
+    cardProduct();
 };
-button.addEventListener('click', function(e){
-    e.preventDefault()
-    let textName = document.getElementById('textName');
-    let textPrice = document.getElementById('textPrice');
-    let input_Number = document.getElementById('input-Number');
-    
-    //=====input data to array=====//
-    array.push(textName.value);
-    array.push(textPrice.value);
-    array.push(input_Number.value);
-    save()
-
-    if(textName.value !=''&& textPrice.value !="" && input_Number.value !=""){
-        let foot_card = document.getElementById('card');
-
-        let card = document.createElement('div');
-        card.className = 'card';
-        card.setAttribute('id','card');
-        
-        let span_h3 = document.createElement('span');
-        let h3 = document.createElement('h3');
-        h3.textContent = textName.value;
-    
-        let b = document.createElement('b');
-        b.textContent = '$'+ textPrice.value;
-    
-        let image = document.createElement('img');
-        image.src = '../Images/delete.jpg';
-    
-        let inpNumber = document.createElement('span');
-        inpNumber.textContent = input_Number.value;
-        let addnumber = document.createElement('img');
-        addnumber.src = '../Images/add.png' ;
-        let reMoveNumber = document.createElement('img');
-        reMoveNumber.src = '../Images/remove.png';
-        
-        span_h3.appendChild(h3);
-        card.appendChild(span_h3);
-        card.appendChild(b);
-        card.appendChild(reMoveNumber);
-        card.appendChild(inpNumber);
-        card.appendChild(addnumber);
-        card.appendChild(image);
-        foot_card.appendChild(card)
-        addcard.appendChild(foot_card);
-
-        textName.value= ''
-        textPrice.value = ''
-        input_Number.value = ''
-    
-        //=====add number======//
-        addnumber.addEventListener('click', function(){
-            sum+=1;
-            inpNumber.textContent=sum;
-        });
-        
-        //=======remove number ======//
-        reMoveNumber.addEventListener('click', function(){
-            if(sum > 0){
-                sum-=1;
-                inpNumber.textContent=sum;
-            };
-        });
-        
-        //========Delete card==========//
-        image.addEventListener('click',function() {
-            if (confirm('Are you delete ?')) {
-                card.remove();
-            };
-            
-        });
-        
-    }else{
-        alert('You have not filled in correctly');
+function getStorage() {
+    if (JSON.parse(localStorage.getItem('deta_Product')) != null) {
+        deta_Product = JSON.parse(localStorage.getItem('deta_Product'));
+        cardProduct();
     };
-});
+};
+
+let text_name = document.getElementById('textName');
+let input_Number = document.getElementById('input-Number');
+let text_Price = document.getElementById('textPrice');
+
+let buttonProduct = document.getElementById('Add-Prooduct');
+buttonProduct.addEventListener('click', Add_Product);
+function Add_Product(){
+    let cards = {
+        name: text_name.value,
+        number: input_Number.value,
+        print: text_Price.value
+    };
+    deta_Product.push(cards);
+    
+    text_name.value = '';
+    input_Number.value = '';
+    text_Price.value = '';
+    saveStorage();
+    
+};
+
+let tbody = document.createElement('tbody');
+let tb = document.querySelector('#table');
+function cardProduct(){
+    tbody.innerHTML = '';
+    for(addCard of deta_Product){
+        let trProduct = document.createElement('tr');
+        let tdProduct1 = document.createElement('td');
+        tdProduct1.textContent = addCard.name;
+        let tdProduct2 = document.createElement('td');
+        tdProduct2.textContent = addCard.number + '$';
+        tdProduct2.style.color = 'red'
+        let tdProduct3 = document.createElement('td');
+        tdProduct3.textContent = addCard.print;
 
 
-// =====================add Categry==================//
+        let tdProduct5 = document.createElement('td')
+        let imgAdd = document.createElement('img');
+        imgAdd.style.width = '25px';
+        imgAdd.src = "../Images/add.png";
+        tdProduct5.appendChild(imgAdd);
+        let tdProduct6 = document.createElement('td');
+        let imgRemovr = document.createElement('img');
+        imgRemovr.style.width = '20px';
+        imgRemovr.src = "../Images/remove.png";
+        tdProduct6.appendChild(imgRemovr);
 
-let img_categry = document.getElementById('imgCateg');
-img_categry.addEventListener('click',categry);
-function categry(){
+
+        let tdProduct4 = document.createElement('td');
+        let deleteProduct = document.createElement('img');
+        deleteProduct.style.width = '20px';
+        deleteProduct.src="../Images/delete.jpg";
+        tdProduct4.appendChild(deleteProduct);
+        deleteProduct.addEventListener('click',delete_Product);
+            
+        trProduct.appendChild(tdProduct1);
+        trProduct.appendChild(tdProduct2);
+        trProduct.appendChild(tdProduct6);
+        trProduct.appendChild(tdProduct3);
+        trProduct.appendChild(tdProduct5);
+        trProduct.appendChild(tdProduct4);
+        
+        tbody.appendChild(trProduct);
+        tb.appendChild(tbody);   
+    };
+};
+
+//========Delete cardProduct==========//
+function delete_Product(e){
+    let datas = JSON.parse(localStorage.getItem('deta_Product'));
+    let product = e.target.parentElement.parentElement;
+    for (let i = 0; i<datas.length; i++){
+        if (datas[i].name === product.children[0].textContent && datas[i].print === product.children[3].textContent){
+            datas.splice(i, 1)
+            localStorage.setItem('deta_Product', JSON.stringify(datas));
+            window.location.reload();
+        };
+    };
+};
+
+
+//======research Product=======//
+
+let inputProduct = document.querySelector('#searchbar');
+inputProduct.addEventListener('input', searchName);
+function searchName(){
+    let result = inputProduct.value.toLowerCase();
+    for (let user of tbody.children){
+        if (user.firstElementChild.textContent.toLowerCase().includes(result)){
+            user.style.display = '';
+        }else{
+            user.style.display = 'none';
+        };
+    };
+};
+
+//=======deply button=====//
+let img = document.getElementById('imgCateg')
+img.addEventListener('click',categ);
+function categ(){
     let h1ID = document.getElementById('h1');
     h1ID.style.display= '';
-    let table = document.querySelector('table');
-    table.style.display = '';
+    let item_search = document.getElementById('item-search');
+    item_search.style.display= '';
+    let img_Categry =document.getElementById('img-Categry');
+    img_Categry.style.display= '';
     let card_categry = document.getElementById('card-categry');
-    card_categry.style.display = ''
-    let imgCateg = document.getElementById('img-Categry');
-    imgCateg.style.display= '';
-    let item_search_categry = document.getElementById('item-search');
-    item_search_categry.style.display = '';
-    let fooder_bloock = document.querySelector('.fooder-none');
-    fooder_bloock.style.display = ''; 
+    card_categry.style.display= '';
+    let table_Product = document.getElementById('tableProduct');
+    table_Product.style.display= '';
 
-    // ==== none page produce===//
     let Total = document.getElementById('Total');
     Total.style.display= 'none';
     let fooder_Card = document.querySelector('.fooderCard');
@@ -113,30 +132,24 @@ function categry(){
     img_Categ.style.display= 'none';
     let im = document.getElementById('im');
     im.style.display = 'none';
-    let bloock = document.querySelector('.fooder-none');
-    bloock.style.display = 'none'; 
+
 };
 
-
-//====to add produce===//
 let image_cate = document.getElementById('img-Categry');
-image_cate.addEventListener('click',addProduce);
-function addProduce(){
+image_cate.addEventListener('click',categry);
+function categry(){
+    
     let h1ID = document.getElementById('h1');
     h1ID.style.display= 'none';
-    let table = document.querySelector('table');
-    table.style.display = 'none';
+    let item_search = document.getElementById('item-search');
+    item_search.style.display= 'none';
+    let img_Categry =document.getElementById('img-Categry');
+    img_Categry.style.display= 'none';
     let card_categry = document.getElementById('card-categry');
-    card_categry.style.display = 'none'
-    let imgCateg = document.getElementById('img-Categry');
-    imgCateg.style.display= 'none';
-    let item_search_categry = document.getElementById('item-search');
-    item_search_categry.style.display = 'none';
+    card_categry.style.display= 'none';
+    let table_Product = document.getElementById('tableProduct');
+    table_Product.style.display= 'none';
 
-    let food_none = document.querySelector('.fooder-none');
-    food_none.style.display = 'none'; 
-
-    // ==== none page produce===//
     let Total = document.getElementById('Total');
     Total.style.display= '';
     let fooder_Card = document.querySelector('.fooderCard');
@@ -147,64 +160,106 @@ function addProduce(){
     img_Categ.style.display= '';
     let im = document.getElementById('im');
     im.style.display = '';
-
-    let fooder_bloock = document.querySelector('.fooder-none');
-    fooder_bloock.style.display = ''; 
 };
 
-
-
-// ===================add table thead tr td============//
-let tr_thead = document.querySelector('tr');
-let table = document.querySelector('table');
-
-let bnt = document.getElementById('Add-Categry');
-bnt.addEventListener('click', addCategry);
-function addCategry(){
-    let textID = document.getElementById('textID');
-    let categoryName = document.getElementById('categoryName');
-    let action = document.getElementById('action');
-
-
-    if(textID.value !=''&& categoryName.value !="" && action.value !=""){
-        let tbody = document.createElement('tbody');
-        let tr = document.createElement('tr');
-        let td1 = document.createElement('td');
-        td1.textContent = textID.value;
-        let td2 = document.createElement('td');
-        td2.textContent = categoryName.value;
-        let td3 = document.createElement('td');
-        td3.textContent = action.value;
-        let td4 = document.createElement('td');
-        let td_delete = document.createElement('img');
-        td_delete.src="../Images/delete.jpg";
-        td_delete.style.width = '20px';
-        td4.appendChild(td_delete);
-        
-        tr.appendChild(td1);
-        tr.appendChild(td2);
-        tr.appendChild(td3);
-        tr.appendChild(td4)
-        tbody.appendChild(tr);
-        table.appendChild(tbody);
-
-        textID.value = '';
-        categoryName.value = '';
-        action.value = '';
-
-        td_delete.addEventListener('click',function(){
-            if (confirm('Are you delete ?')) {
-                tr.remove();
-            };
-        });
-    }else{
-        alert('You have not filled in correctly');
+//============deta===========//
+let deta_Category = [];
+function save() {
+    localStorage.setItem('deta_Category', JSON.stringify(deta_Category));
+    addcardcategry();
+};
+function get() {
+    if (JSON.parse(localStorage.getItem('deta_Category')) != null) {
+        deta_Category = JSON.parse(localStorage.getItem('deta_Category'));
+        addcardcategry();
     };
-    
 };
 
+let category_Name = document.getElementById('categoryName');
+let action = document.getElementById('action');
 
-// =======Search ======//
+let Add_Categry = document.getElementById('Add-Categry');
+Add_Categry.addEventListener('click', createCard);
+function createCard() {
+    let uniqesID = localStorage.getItem('id');
+    if (uniqesID === null) {
+        uniqesID = 1;
+        localStorage.setItem('id', JSON.stringify(uniqesID));
+    } else {
+        uniqesID = parseInt(uniqesID) + 1;
+        localStorage.setItem('id', JSON.stringify(uniqesID));
+    };
+    let cardcategry = {
+        Id: uniqesID,
+        name: category_Name.value,
+        Taste: action.value
+    };
+    category_Name.value ='';
+    action.value ='';
+    deta_Category.push(cardcategry);
+    save();
+};
 
+let tbody_Category = document.createElement('tbody');
+let table_category = document.querySelector('table');
+function addcardcategry(){
+    tbody_Category.innerHTML = '';
+    // tbody_Category  = document.createElement('tbody');
+    for(categ of deta_Category){
+        let trCategory = document.createElement('tr');
+        let tdCategory1 = document.createElement('td');
+        tdCategory1.textContent = categ.Id;
+        let tdCategory2 = document.createElement('td');
+        tdCategory2.textContent = categ.name;
+        let tdCategory3 = document.createElement('td');
+        tdCategory3.textContent = categ.Taste;
+        let tdCategory4 = document.createElement('td');
+        let Cate = document.createElement('img');
+        Cate.style.width = '20px';
+        Cate.src="../Images/delete.jpg";
+        Cate.addEventListener('click',chick_category) 
+        tdCategory4.appendChild(Cate);
+
+        trCategory.appendChild(tdCategory1);
+        trCategory.appendChild(tdCategory2);
+        trCategory.appendChild(tdCategory3);
+        trCategory.appendChild(tdCategory4);
+        
+        tbody_Category.appendChild(trCategory);
+        table_category.appendChild(tbody_Category);
+        
+    };
+};
+
+// ========Delete card==========//
+function chick_category(e){
+    let category = JSON.parse(localStorage.getItem('deta_Category'));
+    let cate = e.target.parentElement.parentElement;
+    for (let i = 0; i < category.length; i++){
+        if (category[i].name === cate.children[1].textContent && category[i].Taste === cate.children[2].textContent){
+            category.splice(i, 1)
+            localStorage.setItem('deta_Category', JSON.stringify(category));
+            window.location.reload();
+        };
+    };
+};
+
+//======research Category=======//
+
+let inputCategpry = document.querySelector('#search-Categry');
+inputCategpry.addEventListener('input', searchNameCategory);
+function searchNameCategory(){
+    let resul = inputCategpry.value.toLowerCase();
+    for (let users of tbody_Category.children){
+        if (users.children[1].textContent.toLowerCase().includes(resul)){
+            users.style.display = '';
+        }else{
+            users.style.display = 'none';
+        };
+    };
+};
+
+get();
+getStorage();
 
 
